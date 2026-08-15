08beb18d28bc27fadf29683ad55a5fb642f005da
Martin Leduc <31558169+DecimalTurn@users.noreply.github.com>
2026-08-15 16:44:37 -0400

    dev: use index.js during e2e testing

--- e2e.config.mjs
+++ e2e.config.mjs
@@ -4,12 +4,15 @@ export default {
 	testMatch: ["**/tests/**/*.e2e.ts"],
 	testPathIgnorePatterns: ["/node_modules/", "/lib/", "<rootDir>/worktrees/"],
 	modulePathIgnorePatterns: ["<rootDir>/worktrees/"],
-	transformIgnorePatterns: ["/node_modules/(?!.*(?:@decimalturn/toml-patch|env-paths|is-safe-filename)/)"],
+	transformIgnorePatterns: [
+		"/node_modules/(?!.*(?:@decimalturn/toml-patch|env-paths|is-safe-filename)/)",
+		"<rootDir>/lib/"
+	],
 	transform: {
 		"^.+\\.[tj]sx?$": ["ts-jest", { tsconfig: "tests/tsconfig.json", diagnostics: false }]
 	},
 	moduleNameMapper: {
-		"^vbapm$": "<rootDir>/src/index.ts",
+		"^vbapm$": "<rootDir>/lib/index.js",
 		"^@timhall/dedent$": "<rootDir>/node_modules/@timhall/dedent/dist/dedent.js"
 	},
 	snapshotFormat: {
